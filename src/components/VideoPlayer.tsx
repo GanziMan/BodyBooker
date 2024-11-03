0e228a6f83596ab3b51381fbcbf9d2f08dc0be9d
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2024-11-03 12:18:03 +0900

    ADD: VideoPlayer 컴포넌트 생성

---
+++ src/components/VideoPlayer.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+interface VideoPlayerProps {
+  videoSrc: string;
+}
+
+const VideoPlayer: React.FC<VideoPlayerProps> = ({ videoSrc }) => {
+  return (
+    <div style={{ position: "relative", overflow: "hidden" }}>
+      <video
+        src={videoSrc}
+        autoPlay
+        loop
+        muted
+        style={{
+          width: "100vw",
+          height: "100vh",
+          objectFit: "cover",
+        }}
+      />
+    </div>
+  );
+};
+
+export default VideoPlayer;
