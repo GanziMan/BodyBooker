b022364a8b1ce24cc17060517d6ebf0dec095351
GanziMan <qjatn50089@gmail.com>
2024-11-08 01:02:32 +0900

    Fix: carousel max-w 수정

--- src/app/(main)/home/HomeCarouselForm.tsx
+++ src/app/(main)/home/HomeCarouselForm.tsx
@@ -14,10 +14,7 @@ import Image from "next/image";
 export default function HomeCarouselForm() {
   return (
     <div className="w-full grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen gap-16 font-[family-name:var(--font-geist-sans)]">
-      <Carousel
-        className="w-full max-w-xs"
-        plugins={[Autoplay({ delay: 2000 })]}
-      >
+      <Carousel className="w-full" plugins={[Autoplay({ delay: 2000 })]}>
         <CarouselContent>
           {Array.from({ length: 16 }).map((_, index) => (
             <CarouselItem key={index}>
