175dba4c57a5d9090dbb5f22bc235c4226078574
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2024-11-07 18:07:14 +0900

    ADD: action schema 작성

--- src/app/(main)/reservation/schema.ts
+++ src/app/(main)/reservation/schema.ts
@@ -7,9 +7,31 @@ export const createReservationSchema = z.object({
 
 export type CreateReservationRequest = z.input<typeof createReservationSchema>;
 
-export type CreateReservationResponse =
+export type CreateReservationResponse = {
+  code: 400 | 200;
+  message: string;
+};
+
+export const pageReservationSchema = z.object({
+  size: z.number().default(10),
+  page: z.number(),
+});
+
+export type ReservationDto = {
+  id: number;
+  userName: string;
+  phone: string;
+  reservationDate: string;
+};
+export type PageReservationRequest = z.input<typeof pageReservationSchema>;
+
+export type PageReservationResponse =
   | {
-      code: 400;
+      code: 400 | 500;
       message: string;
     }
-  | { code: 200; message: string };
+  | {
+      code: 200;
+      message: string;
+      data: ReservationDto[];
+    };
