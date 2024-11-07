c6da04818c56bf6c8f4e628854dd81636174f724
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2024-11-07 18:07:04 +0900

    ADD: 예약 목록 작업

--- src/app/(main)/reservation/ReservationForm.tsx
+++ src/app/(main)/reservation/ReservationForm.tsx
@@ -1,22 +1,96 @@
 "use client";
 import { ko } from "date-fns/locale";
 import { Calendar } from "@/components/ui/calendar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { DialogComponent } from "@/components/Dialog";
+import { useInView } from "react-intersection-observer";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { pageReservation } from "./action";
+
+interface ReservationDataType {
+  id: number;
+  userName: string;
+  phone: string;
+  reservationDate: string;
+
+  // 필요한 필드 추가
+}
 
 export default function ReservationForm() {
   const [date, setDate] = useState<Date | undefined>(new Date());
+  const [reservations, setReservations] = useState<ReservationDataType[]>([]);
+  const [page, setPage] = useState<number>(1);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [hasMore, setHasMore] = useState<boolean>(true);
+
+  const { ref, inView } = useInView();
+
+  useEffect(() => {
+    if (inView && hasMore && !loading) {
+      fetchReservations();
+    }
+  }, [inView]);
+
+  const fetchReservations = async () => {
+    setLoading(true);
+    const result = await pageReservation({ page });
+
+    if (result.code === 200) {
+      setReservations((prev) => [...prev, ...result.data]);
+      setPage((prev) => prev + 1);
+
+      if (result.data.length >= 10) {
+        setHasMore(result.data.length >= 10);
+      }
+    } else {
+      setHasMore(false);
+    }
+  };
 
   return (
-    <Calendar
-      title="예약"
-      locale={ko}
-      selected={date}
-      footer={<DialogComponent date={date!} />}
-      onDayClick={(day) => {
-        setDate(day);
-      }}
-      onSelect={setDate}
-    />
+    <section>
+      <Calendar
+        title="예약"
+        locale={ko}
+        selected={date}
+        footer={<DialogComponent date={date!} />}
+        onDayClick={(day) => {
+          setDate(day);
+        }}
+        onSelect={setDate}
+      />
+      <Table>
+        <TableCaption>예약 명단</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">id</TableHead>
+            <TableHead>이름</TableHead>
+            <TableHead>번호</TableHead>
+            <TableHead className="text-right">예약날짜</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {reservations.map((reservation) => (
+            <TableRow key={reservation.id}>
+              <TableCell className="font-medium">{reservation.id}</TableCell>
+              <TableCell>{reservation.userName}</TableCell>
+              <TableCell>{reservation.phone}</TableCell>
+              <TableCell className="text-right">
+                {reservation.reservationDate}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <div ref={ref} style={{ height: "1px" }}></div>
+    </section>
   );
 }
--- src/app/(main)/reservation/page.tsx
+++ src/app/(main)/reservation/page.tsx
@@ -1,5 +1,5 @@
 import ReservationForm from "./ReservationForm";
 
-export default function Page() {
+export default async function Page() {
   return <ReservationForm />;
 }
