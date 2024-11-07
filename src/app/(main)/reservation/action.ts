99436ca3c458d57fd52b08de64ef186d480a279f
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2024-11-07 12:09:39 +0900

    Fix: Action 및 Schema, Model 예약폼작업

--- src/app/(main)/reservation/action.ts
+++ src/app/(main)/reservation/action.ts
@@ -1,21 +1,38 @@
-"use client";
+"use server";
 
 import { z } from "zod";
 import { PrismaClient as MysqlPrismaClient } from "@prisma/client";
 import { CreateReservationRequest, createReservationSchema } from "./schema";
 
-export async function createReservation(request: CreateReservationRequest) {
+export type CreateReservationResponse =
+  | {
+      code: 400;
+      message: string;
+    }
+  | { code: 200; message: string };
+export async function createReservation(
+  request: CreateReservationRequest
+): Promise<CreateReservationResponse> {
   const validated = await createReservationSchema.safeParseAsync(request);
 
   if (!validated.success) {
-    return {};
+    return {
+      code: 400,
+      message: "파라미터 오류가 있습니다.",
+    };
   }
   const prisma = new MysqlPrismaClient();
 
-  const createResponse = await prisma.reservation.create({
+  await prisma.reservation.create({
     data: {
-      userName: "",
+      userName: validated.data.name,
+      phone: validated.data.phone,
       date: new Date(),
     },
   });
+
+  return {
+    code: 200,
+    message: "생성하였습니다.",
+  };
 }
