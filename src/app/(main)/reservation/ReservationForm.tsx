5c37908d83e318b140a6b9df52f37acf81ab7e9c
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2024-11-07 15:21:22 +0900

    FIx: 사용하지 않는 코드 삭제

--- src/app/(main)/reservation/ReservationForm.tsx
+++ src/app/(main)/reservation/ReservationForm.tsx
@@ -3,24 +3,6 @@ import { ko } from "date-fns/locale";
 import { Calendar } from "@/components/ui/calendar";
 import { useState } from "react";
 import { DialogComponent } from "@/components/Dialog";
-import {
-  DateAfter,
-  DateBefore,
-  DateInterval,
-  DateRange,
-  DayOfWeek,
-} from "react-day-picker";
-
-type Matcher =
-  | boolean
-  | ((date: Date) => boolean)
-  | Date
-  | Date[]
-  | DateRange
-  | DateBefore
-  | DateAfter
-  | DateInterval
-  | DayOfWeek;
 
 export default function ReservationForm() {
   const [date, setDate] = useState<Date | undefined>(new Date());
