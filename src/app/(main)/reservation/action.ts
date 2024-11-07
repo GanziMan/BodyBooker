6cccfca7d97ba41958775697c9746565c3d13ecd
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2024-11-07 18:05:21 +0900

    ADD: pageReservation

--- src/app/(main)/reservation/action.ts
+++ src/app/(main)/reservation/action.ts
@@ -1,12 +1,17 @@
 "use server";
 
-import { z } from "zod";
 import { PrismaClient as MysqlPrismaClient } from "@prisma/client";
+import dayjs from "dayjs";
 import {
   CreateReservationRequest,
   CreateReservationResponse,
   createReservationSchema,
+  PageReservationRequest,
+  PageReservationResponse,
+  pageReservationSchema,
 } from "./schema";
+
+const prisma = new MysqlPrismaClient();
 
 export async function createReservation(
   request: CreateReservationRequest
@@ -19,7 +24,6 @@ export async function createReservation(
       message: validated.error.issues[0].message,
     };
   }
-  const prisma = new MysqlPrismaClient();
 
   await prisma.reservation.create({
     data: {
@@ -33,4 +37,57 @@ export async function createReservation(
     code: 200,
     message: "생성하였습니다.",
   };
+}
+
+export async function pageReservation(
+  request: PageReservationRequest
+): Promise<PageReservationResponse> {
+  const validated = await pageReservationSchema.safeParseAsync(request);
+
+  if (!validated.success) {
+    return {
+      code: 400,
+      message: validated.error.issues[0].message,
+    };
+  }
+
+  const { size, page } = validated.data;
+
+  const skip = (page - 1) * size;
+
+  try {
+    const data = await prisma.reservation.findMany({
+      orderBy: { createdAt: "asc" },
+      take: size,
+      skip: skip,
+      select: {
+        id: true,
+        userName: true,
+        phone: true,
+        date: true,
+      },
+    });
+
+    const formattedData = data.map((item) => {
+      return {
+        id: item.id,
+        userName: item.userName,
+        phone: item.phone,
+        reservationDate: dayjs(item.date).format("YYYY년MM월DD일").toString(),
+      };
+    });
+
+    return {
+      code: 200,
+      message: "조회하였습니다.",
+      data: formattedData,
+    };
+  } catch (e) {
+    console.error(e);
+
+    return {
+      code: 500,
+      message: "Server Error",
+    };
+  }
 }
