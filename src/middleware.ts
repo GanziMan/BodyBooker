4ff4fe2e7e01c6dfd859cd5c1f4a9c7d4edb2fb7
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2024-11-08 19:48:17 +0900

    ADD: middleware 추가

---
+++ src/middleware.ts
@@ -0,0 +1,9 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export default async function middleware(req: NextRequest) {
+  const pathName = req.nextUrl.pathname;
+
+  if (pathName === "/profile") {
+    return NextResponse.redirect("http://localhost:3000/home");
+  }
+}
