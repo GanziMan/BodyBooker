b1aef8fb6c00599a473f77370f7ea06162159831
GanziMan <qjatn50089@gmail.com>
2024-11-11 00:55:37 +0900

    Update: config server.ts 추가 및 middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
+import { DOMAIN_URL } from "./app/config/server";
 
 export default async function middleware(req: NextRequest) {
   const pathName = req.nextUrl.pathname;
 
   if (pathName === "/profile" || pathName === "/") {
-    return NextResponse.redirect("/home");
+    return NextResponse.redirect(DOMAIN_URL + "/home");
   }
 }
