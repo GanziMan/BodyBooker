abba617e7a9d1bdc306ffede318083daebe6fec4
GanziMan <qjatn50089@gmail.com>
2024-11-10 23:03:58 +0900

    Fix: middleWare 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 export default async function middleware(req: NextRequest) {
   const pathName = req.nextUrl.pathname;
 
-  if (pathName === "/profile") {
+  if (pathName === "/profile" || pathName === "/") {
     return NextResponse.redirect("http://localhost:3000/home");
   }
 }
