1d7e9e59c40b11065caa083ca5851caa6706a62d
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2024-11-12 12:33:54 +0900

    ADD: VideoPlayer 추가

--- src/app/(main)/home/page.tsx
+++ src/app/(main)/home/page.tsx
@@ -1,10 +1,10 @@
-// import VideoPlayer from "@/components/VideoPlayer";
+import VideoPlayer from "@/components/VideoPlayer";
 import HomeCarouselForm from "./HomeCarouselForm";
 
 export default function Page() {
   return (
     <section>
-      {/* <VideoPlayer videoSrc="/videos/bodyBookerMain.mp4" /> */}
+      <VideoPlayer videoSrc="/videos/bodyBookerMain.mp4" />
       <HomeCarouselForm />
     </section>
   );
