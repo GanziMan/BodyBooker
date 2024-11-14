ab1c023701795095edcbf3c14aa44f0be32b654d
HandsomeBoy <87753974+GanziMan@users.noreply.github.com>
2024-11-14 17:40:59 +0900

    ADD: Footer 추가

--- src/app/(main)/home/page.tsx
+++ src/app/(main)/home/page.tsx
@@ -6,7 +6,6 @@ export default function Page() {
   return (
     <section className="flex flex-col gap-10">
       <VideoPlayer videoSrc="/videos/bodyBookerMain.mp4" />
-
       <HomeImagesForm />
     </section>
   );
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,20 +1,38 @@
 "use client";
 
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
 
 export default function Footer() {
+  const router = useRouter();
   return (
-    <section className="w-full bg-[#262222E6] h-[281px]">
-      <div></div>
-      <div>
-        <div>
-          <Image
-            src={"/images/icon/instagram-icon.svg"}
-            width={50}
-            height={50}
-            alt="인스타그램 ic"
-          />
+    <section className="w-full bg-[#262222E6] h-[281px] flex justify-center">
+      <div className="text-white flex justify-between max-w-[1280px] w-full px-20 py-10">
+        <div className="flex justify-between w-full">
+          <ul className="flex flex-col gap-2">
+            <li onClick={() => router.push("/home")}>Home</li>
+            <li onClick={() => router.push("/introduce")}>About</li>
+            <li onClick={() => router.push("/reservation")}>Reservation</li>
+            <li onClick={() => toast("준비중인 서비스입니다.")}>PhotoBook</li>
+            <li
+              onClick={() =>
+                window.open("https://open.kakao.com/o/scA4o2Xg", "_blank")
+              }
+            >
+              연락 및 상담 링크
+            </li>
+          </ul>
+          <div>
+            <Image
+              src={"/images/icon/instagram-icon.svg"}
+              width={30}
+              height={30}
+              alt="인스타그램 ic"
+            />
+          </div>
         </div>
+        <div></div>
       </div>
     </section>
   );
